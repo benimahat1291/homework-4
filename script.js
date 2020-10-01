d85fac9897d320e67d2067308f3d9d5077ec7841
Beni Mahat <benimahat6565@gmail.com>
2020-09-30 23:35:33 -0600

    Functioning quiz, need to add Submit form

--- script.js
+++ script.js
@@ -9,29 +9,33 @@ const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
 const scoreSheet = document.getElementById("score");
+let nameform = document.getElementById("nameform");
+
 
 
 //add event litsener for Start button
 start.addEventListener("click", Qstart);
+restart.addEventListener("click", Qrestart);
+
 
 //create our questions as ojbects in an array
 let questions = [
     {
-        question: "1+1=?",
+        question: "Q1:  1+1=?",
         choiceA:"2",
         choiceB:"1",
         choiceC:"3",
         correct:"A",
         
     }, {
-        question: "1+2=?",
+        question: "Q2:  1+2=?",
         choiceA:"1",
         choiceB:"3",
         choiceC:"5",
         correct:"B",
         
     }, {
-        question: "1+3=?",
+        question: "Q3:  1+3=?",
         choiceA:"3",
         choiceB:"5",
         choiceC:"4",
@@ -45,7 +49,7 @@ const lastQ = questions.length -1;
 let questionIndex = 0;
 let clock;
 let score = 0;
-let count = 10;
+let count = 11;
 
 // Question render function
 function Qrender(){
@@ -56,65 +60,86 @@ function Qrender(){
     choiceA.innerHTML = thisQuestion.choiceA;
     choiceB.innerHTML = thisQuestion.choiceB;
     choiceC.innerHTML = thisQuestion.choiceC;
+};
+
+
     
-};
-
-
-//Mark up counter and decress by 1 
-function Qcounter(){
-    if(count >= 0){
-        counter.innerHTML = count;
-        count = count-1; 
-    };
-};
 
 
 function Qstart(){
+    count = 10;
+    clock = setInterval(Qcounter,1000); // repeate Qcounter ever 1sec
+    scoreSheet.style.display = "none";
     console.log("game started");
     start.style.display = "none";  //removes the start button from screen  
-    Qcounter(); // initate the timer
     // showScore();
     Qrender(); //move though the questions array to get new questions
     quiz.style.display= "block"; // makes the quiz section appear
-    clock = setInterval(Qcounter,1000); // repeate Qcounter ever 1sec
+
+};
+function Qrestart(){
+    scoreSheet.style.display = "none";
+    score = 0;
+    count = 10;
+    questionIndex =0;
+    console.log("game started");
+    restart.style.display = "none";  //removes the start button from screen  
+    // showScore();
+    Qrender(); //move though the questions array to get new questions
+    quiz.style.display= "block"; // makes the quiz section appear
 
 };
 //
 function checkAnswer(clicked){
 
-    if(count > 0){ // If counter is is still running allow for next next question
-        if(clicked === questions[questionIndex].correct){ //If userinput is the same as the correct attributte in obj run...
-            score++; // add score by 1
-            console.log("score:" + score);
-        }else{
-            count = count + 3; // if not the correct attribute then add count by 3
-        };
-        
-        if(questionIndex < lastQ){
-            //console.log(questionIndex);
-            questionIndex++; // moves to the next question in questions array
-            console.log(questionIndex);
-            console.log(lastQ);
-            Qrender()
-            
-        }else{
-            endOfGame();
-        };
-    }else if(count = 0 ){
-        endOfGame();
+    
+    if(clicked === questions[questionIndex].correct){ //If userinput is the same as the correct attributte in obj run...
+        score++; // add score by 1
+        console.log("score:" + score);
     }else{
-        console.log("error");
+        count = count - 3; // if not the correct attribute then add count by 3
     };
+    
+    if(questionIndex < lastQ){
+        //console.log(questionIndex);
+        questionIndex++; // moves to the next question in questions array
+        console.log(questionIndex);
+        console.log(lastQ);
+        Qrender()
+        
+    }else{
+        count = 11;
+        endOfGame();
+    };
+};
+    
+//Mark up counter and decress by 1 
+function Qcounter(){
+    if(count <= 10 ){
+        if(count >= 0){
+            counter.innerHTML = "Timer: " + count;
+            count = count-1;
+        }else{
+            count = 11;
+            endOfGame();
 
+        }
+
+    }
     
 };
 
-
 function endOfGame(){
-   console.log("end of game");
-   //console.log(questions.length);
-   scoreSheet.innerHTML = "Your score: " + score;
-   quiz.style.display = "none";
-   scoreSheet.style.display = "none";
+
+   console.log("game has ended");
+   nameform.style.display = "block";
+   quiz.style.display= "none";
+   restart.style.display="block";
+   scoreSheet.innerText = "Score :" + score;
    scoreSheet.style.display = "block";
-    } 
+
+}
+
+function submit(){
+ 
+}
