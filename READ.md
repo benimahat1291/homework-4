474ae0be775e6d89acf0a4f07ead7c6dbe7d14ed
Beni Mahat <benimahat6565@gmail.com>
2020-10-02 14:54:12 -0600

    added comments and more to READ

--- READ.md
+++ READ.md
@@ -1,6 +1,68 @@
-Create questions
-create Variables
 
 
-functions:
-Qrender
+## Application##
+
+We were given the task to make a Quiz that completes the criteria given below:
+
+This assignment tests our unerstanding of Creating DOM elements and eventlitseners.
+using fuctions and accessing Local storages.  
+
+required files:(index.html, script.js, style.css)
+
+## Acceptance Criteria
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+```
+
+## PsuedoCode ##
+
+Create HTML framework 
+
+add dom elements into js
+
+create an array with questions and answers
+
+add event litsener for start button initates start funciton
+
+create start functon that starts the quiz
+    change displays to show quiz
+    start timer
+    start questions
+create a questions render function to display messages
+    use indexing to add questions to the page
+create a cheack answer fucntion that checks the answer
+    use conditional statements to check button values
+    add score and - time
+    end function if index is last in array
+create a counter funtion
+    display time when quiz is happening
+    minus the count - 1
+    user setinterval to minus by -1 every 1 sec
+create a restart function:
+    similat to start function but reset vars
+create end game funciton:
+    change the display to show end of game elements
+    home message,submitlable,restart,score
+create a submit fucntion
+    create lable and save to local storge
+
+
+## Imporvements ##
+
+Improve styling!
+
+functionality:
+
+> Improve timer so that we can chose any start time
+> Make the quesitons generate randomly
+> change color of the button to respond to right and worng answer
--- script.js
+++ script.js
@@ -4,12 +4,13 @@
 const quiz = document.getElementById("quiz");
 const start = document.getElementById("start");
 const restart = document.getElementById("restart");
-
 const question = document.getElementById("question");
 const counter = document.getElementById("counter");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
+const directions = document.getElementById("directions");
+
 const scoreSheet = document.getElementById("score");
 const home = document.getElementById("home");
 var nameform = document.getElementById("nameform");
@@ -18,10 +19,9 @@ var submitbtn = document.getElementById("submit");
 
 
 
-//add event litsener for Start button
-start.addEventListener("click", Qstart);
+//add event litsener for Start & restart button
+start.addEventListener("click", Qstart); //when clicked run the function
 restart.addEventListener("click", Qrestart);
-
 
 //create our questions as ojbects in an array
 let questions = [
@@ -57,13 +57,12 @@ const lastQ = questions.length -1;
 let questionIndex = 0;
 let clock;
 let score = 0;
-let count = 11;
+let count = 11; //set to 11 beaecause timer starts when count = 10
 
 // Question render function
 function Qrender(){
     let thisQuestion = questions[questionIndex];//assign each question object to thisQuestion
     //Mark up obj information in their respective DOM element
-    console.log(thisQuestion);
     home.innerText = thisQuestion.number;
     question.innerHTML = "<p>" + thisQuestion.question + "</p>"; 
     choiceA.innerHTML = thisQuestion.choiceA;
@@ -75,26 +74,26 @@ function Qrender(){
     
 
 
-function Qstart(){
+function Qstart(){ // when start button is pressed, run...
     
-    home.innerHTML = "Coding QUIZ!";
-    count = 10;
+    home.innerHTML = "QUIZ!";
+    count = 10;//reset the cloock to 10sec
     clock = setInterval(Qcounter,1000); // repeate Qcounter ever 1sec
-    scoreSheet.style.display = "none";
     console.log("game started");
+    directions.style.display = "none";
     start.style.display = "none";  //removes the start button from screen  
     // showScore();
     Qrender(); //move though the questions array to get new questions
     quiz.style.display= "block"; // makes the quiz section appear
-    submit();
+    submit(); // run submit function
 
 };
 function Qrestart(){
     nameform.style.display = "none";
     scoreSheet.style.display = "none";
-    score = 0;
-    count = 10;
-    questionIndex =0;
+    score = 0;//reset our variables
+    count = 10; //setting to 10 will reactivate the counter
+    questionIndex = 0; //go back to the first question
     console.log("game started");
     restart.style.display = "none";  //removes the start button from screen  
     // showScore();
@@ -102,17 +101,13 @@ function Qrestart(){
     quiz.style.display= "block"; // makes the quiz section appear
 
 };
-//
+
+//This function is called strat from the html. Each button runs with a constant value A B or C
 function checkAnswer(ABC){
-
-    
-
     if(ABC === questions[questionIndex].correct){ //If userinput is the same as the correct attributte in obj run...
         score++; // add score by 1
         console.log("score:" + score);
         localStorage.setItem("score",score);
-        
-        
     }else{
         count = count - 3; // if not the correct attribute then add count by 3
     };
@@ -122,61 +117,56 @@ function checkAnswer(ABC){
         questionIndex++; // moves to the next question in questions array
         console.log(questionIndex);
         console.log(lastQ);
-        Qrender()
-        
+        Qrender()    
     }else{
         count = 11;
         endOfGame();
     };
 };
     
-//Mark up counter and decress by 1 
-function Qcounter(){
+//Mark up counter and decress counter by 1
+function Qcounter(){//counter function is always running for most counter values
     if(count <= 10 ){
         if(count >= 0){
-            counter.innerHTML = "Timer: " + count;
-            count = count-1;
+            counter.innerHTML = "Timer: " + count; //show the timer on the screen
+            count = count-1; 
         }else{
-            count = 11;
+            count = 11;//change the counter value to outside the condition to stop timer
             endOfGame();
-
         }
-
-    }
-    
+    }   
 };
 
+//when function is called change displays to show and hide different elements
 function endOfGame(){
-    home.innerHTML = "Coding QUIZ!";
+    home.innerHTML = "QUIZ!";
     console.log("game has ended");
     nameform.style.display = "block";
     quiz.style.display= "none";
     restart.style.display="block";
     scoreSheet.innerText = "Score :" + score;
     scoreSheet.style.display = "block";
-
-
-
 }
 
+//Create lable and submit to local storage your score and name
 function submit(){
-    var namebox = document.createElement("input");
-    namebox.setAttribute("placeholder","enter name");
-    nameform.appendChild(namebox);
-    namebox.addEventListener("input", function(e){
+    //create a namebox input with a placeholder to enter name
+    var namebox = document.createElement("input"); //create input document
+    namebox.setAttribute("placeholder","enter name"); // add placeholder text
+    nameform.appendChild(namebox); //appendChild will add new element into nameform ID in mark up
+    namebox.addEventListener("input", function(e){ //tell js to litsen for the input text
         currenttxt = e.target.value
     });
-    submitbtn.addEventListener("click", function(){
-        localStorage.setItem("name",currenttxt);
-        name = localStorage.getItem("name");
-        score = localStorage.getItem("score")
+
+    ///add event litsener to submit but to save result in the namebox
+    submitbtn.addEventListener("click", function(){//when submitbuttin is pressed submit to local storage
+        localStorage.setItem("name",currenttxt); //set currenttxt as name into local storage
+        name = localStorage.getItem("name"); // get from local starge and save txt as name
+        score = localStorage.getItem("score")// do the same for score
         console.log(name, score);
-        var records = document.createElement("p");
+        var records = document.createElement("p");//create a html element to display the submit results
         records.setAttribute("class", "text");
         nameform.appendChild(records);
         records.innerHTML = "Name: " +name + " :  Score: " + score; 
     });
-
-    
- 
 }
